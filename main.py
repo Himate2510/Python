d04702260301b7c8dd2290511414905bb54adfd7
Himate2510 <gamerunkownroblox2500@gmail.com>
2025-03-09 10:02:12 +0530

    Add files via upload
    ACP

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Aryan")
+def calculate_product(numbers):
+    product = 1
+    for number in numbers:
+        product = number
+    return product
+
+
+tuple = (2, 3, 5, 7)
+
+
+result = calculate_product(tuple)
+print("Product of the tuple is... ", result)
