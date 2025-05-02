4a0371484e224c37b2c9f6027ce7e65fd02897cd
Himate2510 <gamerunkownroblox2500@gmail.com>
2025-05-02 17:42:01 +0530

    Add files via upload

---
+++ spaceinvaders.py
@@ -0,0 +1,66 @@
+import math
+import pygame
+import random
+
+sw = 800
+sh = 600
+psx = 400
+psy = 200
+esx = 2
+esy = 20
+collision = 28
+bull_speed = 10
+got = psy - 20
+
+pygame.init()
+screen = pygame.display.set_mode((sw, sh))
+pygame.display.set_caption("Space Invaders")
+
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+bg = pygame.image.load("background.png")
+p_img = pygame.image.load("player.png")
+e_img = pygame.image.load("enemy.png")
+b_img = pygame.image.load("bullet.png")
+
+font = pygame.font.Font("freesansbold.ttf", 32)
+overFont = pygame.font.Font("freesansbold.ttf", 64)
+
+enemy = [
+    {
+        "x": random.randint(0, sw - 64),
+        "y": random.randint(50, 150),
+        "dx": esx
+
+
+    } for _ in range(5)
+]
+
+bx = 0
+by = psy
+bs = "ready"
+
+px, py = psx, psy
+px_c = 0
+
+score = 0
+
+def showscore():
+    score1 = font.render(f"Score: {score}", True, (255,255,255))
+    screen.blit(score1, (10,10))
+
+def over():
+    over1 = overFont.render("Game over! If you won you are good at the game, if not you are bad at it. NO SECOND CHANCES!", True, (255,255,255))
+    screen.blit(over1, (200,300))
+
+def dp():
+    screen.blit(p_img, (px,py))
+
+def ep(enemy):
+    screen.blit(e_img, (enemy["x"], enemy["y"]))
+
+def bull():
+    screen.blit(b_img, (bx + 10, by + 16))
+
+def coll_detect(enemy):
+    return math.hypot(enemy["x"] - bx, enemy["y"] - by) < collision
