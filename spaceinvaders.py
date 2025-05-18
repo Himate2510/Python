7f6c2d8bd8c479001c7fbfa01314c3e1b15e9813
Himate2510 <gamerunkownroblox2500@gmail.com>
2025-05-18 10:37:19 +0530

    Add files via upload

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -5,7 +5,7 @@ import random
 sw = 800
 sh = 600
 psx = 400
-psy = 200
+psy = 500
 esx = 2
 esy = 20
 collision = 28
@@ -64,3 +64,52 @@ def bull():
 
 def coll_detect(enemy):
     return math.hypot(enemy["x"] - bx, enemy["y"] - by) < collision
+running = True
+while running:
+    screen.blit(bg, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                px_c = -5
+            if event.key == pygame.K_RIGHT:
+                px_c = 5
+            if event.key == pygame.K_SPACE and bs == "ready":
+                    bs = "fire"
+                    bx = px
+        if event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+                px_c = 0
+    px = max(0, min(px + px_c, sw - 64))
+    for i in enemy:
+        if i["y"] >= got and abs(i["x"] - px) < collision:
+            for u in enemy:
+                u["y"] = 2000
+            over()
+            pygame.display.update()
+            pygame.time.wait(2000)
+            running = False
+            break
+        i["x"] += i["dx"]
+        if i["x"] <= 0 or i["x"] >= sw - 64:
+            i["dx"] *= -1
+            i["y"] += esy
+        if coll_detect(i):
+            by = psy
+            bs = "ready"
+            score += 1
+            i["x"] = random.randint(0, sw - 64)
+            i["y"] = random.randint(50, 150)
+        ep(i)    
+    if bs == "fire":
+        bull()
+        by -= bull_speed
+        if by <= 0:
+            by = psy
+            bs = "ready"
+    showscore()
+    dp()
+    pygame.display.update()
